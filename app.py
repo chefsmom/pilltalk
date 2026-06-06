bd722ea2a577d249d6e69ad22a08ce509b0d5ade
katie <katiegbowman416@gmail.com>
2026-06-06 16:21:53 -0400

    iOS app redesign - bottom tab bar, native health app feel

--- app.py
+++ app.py
@@ -28,10 +28,6 @@ def symptoms():
 def tracker():
     return send_from_directory("templates", "tracker.html")
 
-@app.route("/app.js")
-def appjs():
-    return send_from_directory("templates", "app.js", mimetype="application/javascript")
-
 @app.route("/pilltalk.js")
 def pilltalkjs():
     return send_from_directory("templates", "pilltalk.js", mimetype="application/javascript")
